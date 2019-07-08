d054196d75df66ca2e0e6cd4215eed46b60e5721
Set <set.alogninouwa@gmail.com>
2019-07-08 17:41:00 +0200

    ajout du fichier contenant le code pour la calculatrice

---
+++ calculatrice.java
@@ -0,0 +1,22 @@
+public class Calculatrice {
+	
+	
+	Static Scanner clavier = new Scanner(System.in);
+	public static void main (String [] args){
+		int nbre1 =0 , nbre2=0;
+		System.out.println("Entrer le nombre1:");
+		nbre1 = clavier.nextInt();
+		
+		
+		System.out.println("Entrer le nombre2:");
+		nbre2 = clavier.nextInt();
+		
+		
+		System.out.println("La somme de "+ nbre1+ " et "+ nbre2 + " est: " (nbre1+nbre2));
+		System.out.println("La différence de "+ nbre1+ " et "+ nbre2 + " est: " (nbre1-nbre2));
+	}
+	
+	
+	
+	
+}
